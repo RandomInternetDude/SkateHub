66839e6b58a6553f941e5c8e56af498e39922162
Joey Rivera <joey7040@hotmail.com>
2018-05-03 16:26:21 +0000

    app.js update

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ var commentRoutes   = require("./routes/comments"),
     skateparkRoutes = require("./routes/skateparks"),
     indexRoutes     = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_skate");
+// mongoose.connect("mongodb://localhost/yelp_skate");
+mongoose.connect("mongodb://joey:<colors>@ds111050.mlab.com:11050/skatehub");
+// mongodb://<joey>:<Downwood2>@ds111050.mlab.com:11050/skatehub
 app.use(bodyParser.urlencoded({extended: true}));
 app.set ("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
