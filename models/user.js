bd8b07e3e4c7f4eb539dda536ba582ba815c9a76
Joey Rivera <joey7040@hotmail.com>
2018-05-07 00:17:52 +0000

    initial commit of user profiles

--- models/user.js
+++ models/user.js
@@ -4,6 +4,10 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String,
+    avater: String,
+    firstName: String,
+    lastName: String,
+    email: String,
 });
 
 UserSchema.plugin(passportLocalMongoose);
--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,18 @@ router.get("/register", function(req, res) {
 });
 // handles sign up form logic
 router.post("/register", function(req, res) {
-    var newUser = new User({username: req.body.username});
+    var newUser = new User(
+        {
+            username: req.body.username, 
+            firstname: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            avatar: req.body.avatar,
+            
+        });
+    // if (req.body.adminCode === "secretCode420"){
+    //     newUser.isAdmin = true;
+    // }
     User.register(newUser,req.body.password, function (err , user){
         if(err){
             req.flash("error", err.message);
@@ -51,6 +62,19 @@ router.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/skateparks");
 });
+
+// user profile route
+router.get("/user/:id", function(req, res){
+   User.findById(req.params.id, function(err, foundUser){
+       if (err){
+           req.flash("error", "Something went wrong.")
+           res.redirect("/skateparks");
+       }
+       res.render("users/show", {user: foundUser});
+   }); 
+});
+ 
+
 
 
 function isLoggedIn(req, res, next){
